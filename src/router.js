cbf81ec6549909b61fdf40c98cea7b541c89a222
Saige <1499340752@qq.com>
2020-05-08 08:50:13 +0800

    config router to history mode,add base; add publicPath to vue.config.js.  all for deploying to nginx server

--- src/router.js
+++ src/router.js
@@ -3,27 +3,34 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
+const routes = [
+  {
+    path: '/index.html',
+    redirect: '/'
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/tools',
+    name: 'tools',
+    component: function () { 
+      return import(/* webpackChunkName: "about" */ './views/Tools.vue')
+    }
+  },
+  {
+    path: '/games',
+    name: 'games',
+    component: function () { 
+      return import(/* webpackChunkName: "about" */ './views/Snake.vue')
+    }
+  }
+]
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/tools',
-      name: 'tools',
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/Tools.vue')
-      }
-    },
-    {
-      path: '/games',
-      name: 'games',
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/Snake.vue')
-      }
-    }
-  ]
+  base:'/statistic/',
+  mode:'history',  //服务器要配置
+  routes, 
 })
